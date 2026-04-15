292c7571b46e9c9156c223cf7ac2f61144701c02
anaclaramtn <anaclaramtn@gmail.com>
2026-04-15 12:15:48 -0300

    tela de cadastro feita

--- app/src/main/java/com/example/biblioteca_app/LoginActivity.kt
+++ app/src/main/java/com/example/biblioteca_app/LoginActivity.kt
@@ -1,14 +1,20 @@
 package com.example.biblioteca_app
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 // como uma nova activity deve ser criada assim que adicionamos uma tela
 //res > new > empty views activity
 
-class tela_cadastro : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tela_cadastro)
+
+        setContentView(R.layout.tela_login)
+
+
     }
 }
