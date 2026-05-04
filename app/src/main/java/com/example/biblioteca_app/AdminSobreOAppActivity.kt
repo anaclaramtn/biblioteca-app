fe7bc9ed55f341ef66568d7d2b893d4c823dc80f
BrunoPonte2005 <brunopontecs2005@gmail.com>
2026-05-04 01:08:24 -0300

    -Alteração da Navbar -Criação da sobre o app do admin -Alteração no botao de voltar pra ir pra configuracoes...

--- app/src/main/java/com/example/biblioteca_app/AdminSobreOAppActivity.kt
+++ app/src/main/java/com/example/biblioteca_app/AdminSobreOAppActivity.kt
@@ -2,52 +2,31 @@ package com.example.biblioteca_app
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class AdminConfiguracoesActivity : AppCompatActivity() {
+class AdminSobreOAppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tela_admin_configuracoes)
+        setContentView(R.layout.tela_admin_sobreoapp)
 
-        // 🔹 HEADER (mesmo padrão das outras telas admin)
-        val header = findViewById<View>(R.id.header)
+        val btnVoltar = findViewById<ImageView>(R.id.btnVoltar)
 
-        val titulo = header.findViewById<TextView>(R.id.txtTitulo)
-        val btnBack = header.findViewById<ImageView>(R.id.btnBack)
-
-        titulo.text = "Configurações"
-
-        btnBack.visibility = View.VISIBLE
-        btnBack.setOnClickListener {
-            startActivity(Intent(this, AdminMenuActivity::class.java))
+        btnVoltar.setOnClickListener {
+            val intent = Intent(this, AdminConfiguracoesActivity::class.java)
+            startActivity(intent)
             finish()
-        }
-
-        // 🔹 BOTÕES
-        val btnAlterarSenha = findViewById<TextView>(R.id.btnAlterarSenha)
-        val btnSobreOApp = findViewById<TextView>(R.id.btnSobreOApp)
-
-        btnAlterarSenha.setOnClickListener {
-            startActivity(Intent(this, AdminAlteracaoSenhaActivity::class.java))
-        }
-
-        btnSobreOApp.setOnClickListener {
-            startActivity(Intent(this, AdminSobreOAppActivity::class.java))
         }
 
         setupNavBar()
     }
 
-    // PADRÃO ADMIN (igual Home/AdminMenu)
     private fun setupNavBar() {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavAdmin)
 
-        bottomNav.selectedItemId = R.id.nav_home
+        bottomNav.selectedItemId = R.id.nav_menu
 
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
