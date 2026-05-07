02a9744ff6131ec61baeb3ab4382d571e3cd5133
João Victor <29joaovictormaciel@gmail.com>
2026-05-07 01:15:18 -0300

    Adiciona os toast da tela de Redefinição de Senha

--- app/src/main/java/com/example/biblioteca_app/TelaRedefinicaoSenhaActivity.kt
+++ app/src/main/java/com/example/biblioteca_app/TelaRedefinicaoSenhaActivity.kt
@@ -2,8 +2,11 @@ package com.example.biblioteca_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class TelaRedefinicaoSenhaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,26 @@ class TelaRedefinicaoSenhaActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        val emailRedefSenha = findViewById<EditText>(R.id.TextInputEditTextEmail)
+        val btnRedefinicao = findViewById<MaterialButton>(R.id.ButtonRedefinicao)
+
+        btnRedefinicao.setOnClickListener {
+            val email = emailRedefSenha.text.toString()
+            if (email == "narakao@gmail.com") {
+                Toast.makeText(
+                    this,
+                    "O codigo foi enviado!\nFavor, verificar a caixa do email!",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Seu email nao existe em nosso banco\nPara prosseguir com a recuperacao de senha,\nfavor, preencher o email correto ",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
